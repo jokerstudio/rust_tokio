447bb19e5d9cbc755a45a58e4494dfd4be265b8d
JokerStudio <jokerstudio26@gmail.com>
2022-10-30 21:03:58 +0700

    Add example chat server

--- README.md
+++ README.md
@@ -2,14 +2,14 @@
 
 Now, start the server:
 ```
-$ cargo run
+cargo run --example redis
 ```
 and in a separate terminal window, run the hello-redis example:
 ```
-$ cargo run --example hello-redis
+cargo run
 ```
 Now, the output will be:
 ```
-result = b"under the dev"
+result = "under the dev"
 ```
 We can now get and set values, but there is a problem: The values are not shared between connections. If another socket connects and tries to GET the hello key, it will not find anything.
---
+++ examples/chat.rs
@@ -0,0 +1,42 @@
+use tokio::{
+    io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
+    net::TcpListener,
+    sync::broadcast,
+};
+
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
+    let (tx, _) = broadcast::channel(10);
+
+    loop {
+        let (mut socket, addr) = listener.accept().await.unwrap();
+        let tx = tx.clone();
+        let mut rx = tx.subscribe();
+
+        tokio::spawn(async move {
+            let (reader, mut writer) = socket.split();
+
+            let mut reader = BufReader::new(reader);
+            let mut line = String::new();
+
+            loop {
+                tokio::select! {
+                    result = reader.read_line(&mut line) => {
+                        if result.unwrap() == 0 {
+                            break;
+                        }
+                        tx.send((line.clone(), addr)).unwrap();
+                        line.clear();
+                    }
+                    result = rx.recv() => {
+                        let (msg, sender_addr) = result.unwrap();
+                        if sender_addr != addr {
+                            writer.write_all(msg.as_bytes()).await.unwrap();
+                        }
+                    }
+                }
+            }
+        });
+    }
+}
